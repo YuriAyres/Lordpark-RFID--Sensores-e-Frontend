01e7d0bc9d210088180fcef401df93abb4c24d20
YuriAyres <yuriadp7@gmail.com>
2024-10-31 00:39:53 -0300

    Correções SPI e Redução de código

--- RFID/rfid_estacionamento.py
+++ RFID/rfid_estacionamento.py
@@ -5,49 +5,54 @@ import signal
 import sys
 import requests
 import spidev
+import threading
 
 # Configurações de hardware
 GPIO.setmode(GPIO.BOARD)
-BUZZER_ENTRADA = 38
-BUZZER_SAIDA = 40  # Novo buzzer para saída
-CS_ENTRADA = 8  # CS do leitor de entrada
-CS_SAIDA = 22   # CS do leitor de saída
-SERVO_ENTRADA_PIN = 10  # Pino do servo da cancela de entrada
-SERVO_SAIDA_PIN = 12    # Pino do servo da cancela de saída
-
-# Pinos dos LEDs
-LED_VERMELHO_ENTRADA = 32  # LED vermelho para entrada
-LED_VERDE_ENTRADA = 36      # LED verde para entrada
-LED_VERMELHO_SAIDA = 33     # LED vermelho para saída
-LED_VERDE_SAIDA = 35        # LED verde para saída
+BUZZER_ENTRADA = 13
+BUZZER_SAIDA = 37  
+CS_ENTRADA = 24  
+CS_SAIDA = 12   
+SERVO_ENTRADA_PIN = 32 
+SERVO_SAIDA_PIN = 33
+LED_VERMELHO_ENTRADA = 16  
+LED_VERDE_ENTRADA = 18    
+LED_VERMELHO_SAIDA = 29   
+LED_VERDE_SAIDA = 31
 
 GPIO.setup(BUZZER_ENTRADA, GPIO.OUT)
 GPIO.setup(BUZZER_SAIDA, GPIO.OUT)
 GPIO.setup(CS_ENTRADA, GPIO.OUT)
 GPIO.setup(CS_SAIDA, GPIO.OUT)
-
-# Configuração do PWM para os servos
 GPIO.setup(SERVO_ENTRADA_PIN, GPIO.OUT)
 GPIO.setup(SERVO_SAIDA_PIN, GPIO.OUT)
+GPIO.setup(LED_VERMELHO_ENTRADA, GPIO.OUT)
+GPIO.setup(LED_VERDE_ENTRADA, GPIO.OUT)
+GPIO.setup(LED_VERMELHO_SAIDA, GPIO.OUT)
+GPIO.setup(LED_VERDE_SAIDA, GPIO.OUT)
+
+# Configuração do PWM para os servos
 servoEntrada = GPIO.PWM(SERVO_ENTRADA_PIN, 50)  # Frequência de 50Hz
 servoSaida = GPIO.PWM(SERVO_SAIDA_PIN, 50)      # Frequência de 50Hz
 servoEntrada.start(0)  # Inicializa em 0 graus
 servoSaida.start(0)    # Inicializa em 0 graus
 
-# Configuração da SPI e inicialização do leitor
-spi = spidev.SpiDev()
-spi.open(0, 0)  # Canal SPI 0, dispositivo 0
-spi.max_speed_hz = 1000000
-leitorRFID = SimpleMFRC522()
+# Configuração da SPI para o leitor de entrada e saída
+spi_entrada = spidev.SpiDev()
+spi_saida = spidev.SpiDev()
+
+# Abre o SPI 0,0 para o leitor de entrada
+spi_entrada.open(0, 0)
+spi_entrada.max_speed_hz = 1000000
+leitorRFID_entrada = SimpleMFRC522()
+
+# Abre o SPI 1,0 para o leitor de saída
+spi_saida.open(1, 0)
+spi_saida.max_speed_hz = 1000000
+leitorRFID_saida = SimpleMFRC522()
 
 # URL da API
 URL_API = "http://10.1.24.62:5000"
-
-# Configuração dos LEDs
-GPIO.setup(LED_VERMELHO_ENTRADA, GPIO.OUT)
-GPIO.setup(LED_VERDE_ENTRADA, GPIO.OUT)
-GPIO.setup(LED_VERMELHO_SAIDA, GPIO.OUT)
-GPIO.setup(LED_VERDE_SAIDA, GPIO.OUT)
 
 # Funções para o buzzer
 def tocar_buzzer(frequencia, duracao, buzzer):
@@ -56,28 +61,15 @@ def tocar_buzzer(frequencia, duracao, buzzer):
     sleep(duracao)
     p.stop()
 
-def buzzer_leitura_feita():
-    tocar_buzzer(500, 0.5, BUZZER_ENTRADA)
+def buzzer_erro(buzzer, ledvermelho, ledverde):
+    tocar_buzzer(200, 0.5, buzzer)
+    GPIO.output(ledvermelho, GPIO.HIGH)  # Liga LED vermelho
+    GPIO.output(ledverde, GPIO.LOW)      # Desliga LED verde
 
-def buzzer_erro_entrada():
-    tocar_buzzer(200, 0.5, BUZZER_ENTRADA)
-    GPIO.output(LED_VERMELHO_ENTRADA, GPIO.HIGH)  # Liga LED vermelho
-    GPIO.output(LED_VERDE_ENTRADA, GPIO.LOW)      # Desliga LED verde
-
-def buzzer_sucesso_entrada():
-    tocar_buzzer(1000, 0.5, BUZZER_ENTRADA)
-    GPIO.output(LED_VERMELHO_ENTRADA, GPIO.LOW)   # Desliga LED vermelho
-    GPIO.output(LED_VERDE_ENTRADA, GPIO.HIGH)     # Liga LED verde
-
-def buzzer_erro_saida():
-    tocar_buzzer(200, 0.5, BUZZER_SAIDA)
-    GPIO.output(LED_VERMELHO_SAIDA, GPIO.HIGH)    # Liga LED vermelho
-    GPIO.output(LED_VERDE_SAIDA, GPIO.LOW)        # Desliga LED verde
-
-def buzzer_sucesso_saida():
-    tocar_buzzer(1000, 0.5, BUZZER_SAIDA)
-    GPIO.output(LED_VERMELHO_SAIDA, GPIO.LOW)     # Desliga LED vermelho
-    GPIO.output(LED_VERDE_SAIDA, GPIO.HIGH)       # Liga LED verde
+def buzzer_sucesso(buzzer, ledvermelho, ledverde):
+    tocar_buzzer(1000, 0.5, buzzer)
+    GPIO.output(ledvermelho, GPIO.LOW)   # Desliga LED vermelho
+    GPIO.output(ledverde, GPIO.HIGH)     # Liga LED verde
 
 # Função para abrir e fechar a cancela
 def abrir_cancela(servo):
@@ -96,23 +88,19 @@ def finalizar_programa(signal, frame):
 
 signal.signal(signal.SIGINT, finalizar_programa)
 
-# Função para leitura com alternância de leitores
-def ler_tag(cs_pin):
+
+def ler_tag(cs_pin, leitor):
     # Ativar CS do leitor desejado
     GPIO.output(cs_pin, GPIO.LOW)
     sleep(0.1)  # Pequeno delay para estabilizar
 
     # Leitura
     try:
-        tag_id, _ = leitorRFID.read()
-        buzzer_leitura_feita()
+        tag_id, _ = leitor.read()
         return int(tag_id)
     except Exception as e:
         print(f"Erro ao ler o RFID: {e}")
-        buzzer_erro_entrada()  # Use o buzzer e LED de erro para entrada
-    finally:
-        # Desativar CS após leitura
-        GPIO.output(cs_pin, GPIO.HIGH)
+        sleep(0.1)
 
     return None
 
@@ -125,23 +113,23 @@ def processar_entrada(tag):
         reserva = carro.get('reserva')
         if placa:
             if reserva == 'reservado':
-                buzzer_sucesso_entrada()  # Sucesso na entrada
+                buzzer_sucesso(BUZZER_ENTRADA, LED_VERMELHO_ENTRADA, LED_VERDE_ENTRADA)  # Sucesso na entrada
                 abrir_cancela(servoEntrada)  # Abre a cancela de entrada
                 response = requests.post(f"{URL_API}/estacionar", json={'carro_id': tag})
                 if response.status_code == 200:
                     print("Veículo registrado com sucesso na entrada.")
                 else:
                     print("Erro ao registrar entrada.")
-                    buzzer_erro_entrada()
+                    buzzer_erro(BUZZER_ENTRADA, LED_VERMELHO_ENTRADA, LED_VERDE_ENTRADA)
             else:
                 print("Erro ao registrar entrada, veículo não tem reserva.")
-                buzzer_erro_entrada()
+                buzzer_erro(BUZZER_ENTRADA, LED_VERMELHO_ENTRADA, LED_VERDE_ENTRADA)
         else:
             print("ID não reconhecido.")
-            buzzer_erro_entrada()
+            buzzer_erro(BUZZER_ENTRADA, LED_VERMELHO_ENTRADA, LED_VERDE_ENTRADA)
     else:
         print("Erro ao buscar dados do carro.")
-        buzzer_erro_entrada()
+        buzzer_erro(BUZZER_ENTRADA, LED_VERMELHO_ENTRADA, LED_VERDE_ENTRADA)
 
 def processar_saida(tag):
     response = requests.get(f"{URL_API}/carros/{tag}")
@@ -149,35 +137,56 @@ def processar_saida(tag):
         carro = response.json()
         placa = carro.get('placa')
         if placa:
-            buzzer_sucesso_saida()  # Sucesso na saída
+            buzzer_sucesso(BUZZER_SAIDA, LED_VERMELHO_SAIDA, LED_VERDE_SAIDA)  # Sucesso na saída
             abrir_cancela(servoSaida)  # Abre a cancela de saída
             response = requests.post(f"{URL_API}/sair", json={'carro_id': tag})
             if response.status_code == 200:
                 print("Saída registrada com sucesso.")
             else:
                 print("Erro ao registrar saída.")
-                buzzer_erro_saida()
+                buzzer_erro(BUZZER_SAIDA, LED_VERMELHO_SAIDA, LED_VERDE_SAIDA)
         else:
             print("ID não reconhecido.")
-            buzzer_erro_saida()
+            buzzer_erro(BUZZER_SAIDA, LED_VERMELHO_SAIDA, LED_VERDE_SAIDA)
     else:
         print("Erro ao buscar dados do carro.")
-        buzzer_erro_saida()
+        buzzer_erro(BUZZER_SAIDA, LED_VERMELHO_SAIDA, LED_VERDE_SAIDA)
+
+def monitorar_entrada():
+    while True:
+        print("Aguardando leitura na entrada...")
+        tagEntrada = ler_tag(CS_ENTRADA, leitorRFID_entrada)
+        if tagEntrada:
+            processar_entrada(tagEntrada)
+        sleep(1)
+
+def monitorar_saida():
+    while True:
+        print("Aguardando leitura na saída...")
+        tagSaida = ler_tag(CS_SAIDA, leitorRFID_saida)
+        if tagSaida:
+            processar_saida(tagSaida)
+        sleep(1)
 
 # Loop principal
 try:
+    # Iniciando threads para monitorar entrada e saída simultaneamente
+    thread_entrada = threading.Thread(target=monitorar_entrada)
+    thread_saida = threading.Thread(target=monitorar_saida)
+
+    # Tornando as threads daemon para que finalizem quando o programa principal terminar
+    thread_entrada.daemon = True
+    thread_saida.daemon = True
+
+    # Iniciando as threads
+    thread_entrada.start()
+    thread_saida.start()
+
+    # Mantém o programa principal em execução enquanto as threads funcionam
     while True:
-        print("Aguardando leitura na entrada...")
-        tagEntrada = ler_tag(CS_ENTRADA)
-        if tagEntrada:
-            processar_entrada(tagEntrada)
+        sleep(0.1)
 
-        print("Aguardando leitura na saída...")
-        tagSaida = ler_tag(CS_SAIDA)
-        if tagSaida:
-            processar_saida(tagSaida)
-
-        sleep(1)
+        
 
 finally:
     GPIO.cleanup()
